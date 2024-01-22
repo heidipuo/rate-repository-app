c22cbeaa9c43267de2b224a8e6ebe1471cca8593
Heidi <puotiniemi.heidi@gmail.com>
2024-01-22 14:42:41 +0200

    refactoring signIn and useSignIn

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -6,6 +6,7 @@ import * as yup from 'yup'
 import Text from './Text';
 import theme from '../theme';
 import useSignIn from '../hooks/useSignIn';
+import { useNavigate } from "react-router-native";
 
 const styles = StyleSheet.create ({
     flexContainer: {
@@ -59,14 +60,14 @@ const SignInForm = ({ onSubmit }) => {
   
   const SignIn = () => {
     const [signIn] = useSignIn()
-    
+    const navigate = useNavigate()
+
     const onSubmit =  async (values) => {
         const { username, password } = values
     
       try {
-        const { data } = await signIn({ username, password });
-        //AuthStorage.setAccessToken(data.authenticate.accessToken)
-        console.log('form', data.authenticate.accessToken)
+        await signIn({ username, password });
+        navigate("/")
       } catch (e) {
         console.log(e);
       }
--- src/hooks/useSignIn.js
+++ src/hooks/useSignIn.js
@@ -2,32 +2,38 @@ import { useMutation, useApolloClient } from "@apollo/client";
 import { SIGN_IN_USER } from "../graphql/mutations";
 import { useContext } from "react";
 import AuthStorageContext from "../contexts/AuthStorageContext";
-import { useNavigate } from "react-router-native";
+
 
 const useSignIn = () => {
     const authStorage = useContext(AuthStorageContext);
     const [mutate, result] = useMutation(SIGN_IN_USER)
-    const navigate = useNavigate()
+  
     const apolloClient = useApolloClient();
 
     const signIn = async ({ username, password }) => {
       
-      const result = await mutate({ variables: {
+      const payload = await mutate({ variables: {
                 credentials: {
                     password: password,
                     username: username}
           }})
-      navigate("/")
+  
+      console.log('pay', payload)
+     /* const authToken = payload.data
+        ? payload.data.authenticate.accessToken
+        : undefined */
 
-      return result
+      if (payload.data?.authenticate) {      
+        await authStorage.setAccessToken(payload.data.authenticate.accessToken);
+        apolloClient.resetStore();
+      }
+      
+      return payload
         
       };
-      const authToken = result.data
-        ? result.data.authenticate.accessToken
-        : undefined 
+     
       
-      authStorage.setAccessToken(authToken);
-      apolloClient.resetStore();
+     
     
       return [signIn, result];
     };
